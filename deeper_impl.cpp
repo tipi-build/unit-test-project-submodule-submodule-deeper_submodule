6490b854ee1ee5fa54720ed877ae1a9a13b0cea5
Damien Buhl (alias daminetreg) <damien.buhl@lecbna.org>
2023-02-03 00:07:05 +0100

    :gear: this repo is supposed to be one submodule level deeper.

---
+++ deeper_impl.cpp
@@ -0,0 +1,7 @@
+#include <submodule/print_to_console.hpp>
+
+#include <iostream>
+
+void submodule::deeper_submodule_print_to_console() {
+  std::cout << "submodule::deeper_submodule_print_to_console()" << std::endl;
+}
